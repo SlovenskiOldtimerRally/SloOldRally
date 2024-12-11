39b164321165e4539325a1f7a01bdff7194cedc6
maj kikelj <maj.kikelj@gmail.com>
2024-12-11 21:15:54 +0100

    Added club information to profile view

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -1,8 +1,19 @@
 <section>
     <header>
-        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+        <h2 class=" mt-2 text-lg font-medium text-gray-900 dark:text-gray-100">
             {{ __('Profile Information') }}
         </h2>
+
+        <div class="mt-2">
+
+            <x-input-label for="club_id" :value="__('Klub')" />
+            <select disabled name="club" id="club" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                <option value="{{ $user->club_id }}">{{ $user->club->name }}</option>
+
+            </select>
+        </div>
+
+        <hr class="mt-2">
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
             {{ __("Update your account's profile information and email address.") }}
