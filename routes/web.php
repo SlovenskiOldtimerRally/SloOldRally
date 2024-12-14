c207ae4ad0021e51f52c84f38ca6e1005307de55
maj kikelj <maj.kikelj@gmail.com>
2024-12-14 17:38:00 +0100

    Restricted ClubAdmin's access to normal dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EventRegistrationController;
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\denyClubAdmin;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\isClubAdmin;
@@ -23,7 +24,7 @@ Route::get('/register', function(){
 
 
 
-Route::get('dashboard', [DashboardController::class, 'userEvents'],)->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('dashboard', [DashboardController::class, 'userEvents'],)->middleware(['auth', 'verified', denyClubAdmin::class])->name('dashboard');
 Route::get('event/{event}', [DashboardController::class, 'event_detail'],)->name('event-detail');
 
 Route::middleware([isClubAdmin::class])->group(function () {
@@ -33,7 +34,7 @@ Route::middleware([isClubAdmin::class])->group(function () {
 });
 
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', 'verified', denyClubAdmin::class])->group(function () {
     Route::post('dashboard/{event}/register', [EventRegistrationController::class, 'create_registration'])->name('event.register');
     Route::delete('dashboard/{event}/register-delete', [EventRegistrationController::class, 'delete_registration'])->name('event.register.delete');
 });
