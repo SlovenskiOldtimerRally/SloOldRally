3f33e5fea6eb18421298d098c3be29daefe49e06
maj kikelj <maj.kikelj@gmail.com>
2024-12-14 21:20:16 +0100

    start_number assignment for event registrations

--- app/Http/Controllers/EventRegistrationController.php
+++ app/Http/Controllers/EventRegistrationController.php
@@ -15,6 +15,7 @@ class EventRegistrationController extends Controller
 
         $eventRegistration = Registration::where('user_id', $userId)->where('event_id', $event_id)->first();
 
+        $noOfRegistrations = count(Registration::where('event_id', $event_id)->get());
 
         if($eventRegistration != null){
             return redirect()->back()->with('failure', 'Na ta dogodek ste že prijavljeni');
@@ -24,6 +25,7 @@ class EventRegistrationController extends Controller
         $registration = Registration::create([
             'user_id' => $userId,
             'event_id' => $event_id,
+            'start_number' => $noOfRegistrations + 1,
         ]);
 
         return redirect()->back()->with('success', 'Prijava je bila uspešna');
@@ -39,6 +41,14 @@ class EventRegistrationController extends Controller
         if($eventRegistration == null){
             return redirect()->back()->with('failure', 'Odjava ni mogoča saj niste prijavljeni!');
         }
+
+        $allRegistrations = Registration::where('event_id', $event_id)->orderBy('start_number','ASC')->get();
+
+        for($i = $eventRegistration->start_number - 1; $i < count(Registration::where('event_id', $event_id)->get()) ; $i++){
+            $allRegistrations[$i]->start_number--;
+            $allRegistrations[$i]->save();
+        }
+
         $eventRegistration->delete();
 
         return redirect()->back()->with('success', 'Odjava je bila uspešna');
